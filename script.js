8c8aa420667c4ca48cb6b2a3f7c13799078bdb0e
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2024-09-11 01:08:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function changeText()
+{
+    document.getElementById("textOne").innerHTML= "Nice, you clicked the button!"
+}
+
+function niceMessage() 
+{
+    document.getElementById("testing").innerHTML += "You are Lovely!"
+}
+
+function howAmI()
+{
+    let x = document.getElementById("Choises").value;
+    document.getElementById("myChoise").innerHTML = "You think I am doing; " + x;
+}
+
+function goodBye()
+{
+    document.getElementById("bye").innerHTML= "Thanks, bye!"
+}
+
